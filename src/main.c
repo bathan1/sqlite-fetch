4f6e1ef0a7fc989c3363fc370a8bafadfe2e0ccd
bathan1 <side_nathan@hotmail.com>
2025-11-28 14:57:10 -0500

    Extract fetch_pop

--- src/main.c
+++ src/main.c
@@ -21,34 +21,45 @@ ssize_t read_full(int fd, void *buf, size_t len) {
     return off;
 }
 
+char *fetch_pop(int fd, size_t *length) {
+    uint64_t len = 0;
+    for (;;) {
+        ssize_t n = read_full(fd, &len, sizeof(len));
+        if (n == 0) {
+            return NULL;
+        } else if (n < 0) {continue;}
+
+        if (length) {
+            *length = len;
+        }
+        char *obj = malloc(len + 1);
+        if (!obj) {return NULL;}
+
+        ssize_t m = read_full(fd, obj, len);
+        if (m == 0) { free(obj); return NULL; }
+        if (m < 0) { free(obj); continue; }
+        obj[len] = 0;
+        return obj;
+    }
+}
+
 int main() {
     int fd = fetch("http://jsonplaceholder.typicode.com/todos", (fetch_init_t) {0});
 
-    uint64_t len;
+    uint64_t len = 0;
 
     int count = 0;
     while (1) {
-        ssize_t n = read_full(fd, &len, sizeof(len));
-
-        if (n == 0) break;
-        if (n < 0) {
-            continue;
+        char *obj = fetch_pop(fd, &len);
+        if (!obj) {
+            close(fd);
+            break;
         }
-
-        char *obj = malloc(len + 1);
-        if (!obj) break;
-
-        ssize_t m = read_full(fd, obj, len);
-        if (m == 0) { free(obj); break; }
-        if (m < 0) { free(obj); continue; }
         printf("%s\n", obj);
-
-        obj[len] = 0;
         free(obj);
         count++;
     }
 
-    close(fd);
     return 0;
 }
 
